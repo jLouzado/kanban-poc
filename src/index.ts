ed8913c0b7385739faf49a49700ddd375ef89a60
Joel Louzado <jlouzado@gmail.com>
2020-11-11 05:07:15 +0530

    feat(kanban): basic layout

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import faker from "faker";
-import { init } from "snabbdom/init";
-import { classModule } from "snabbdom/modules/class";
-import { propsModule } from "snabbdom/modules/props";
-import { styleModule } from "snabbdom/modules/style";
-import { eventListenersModule } from "snabbdom/modules/eventlisteners";
-import { h } from "snabbdom/h"; // helper function for creating vnodes
+import { init } from "snabbdom/build/package/init";
+import { classModule } from "snabbdom//build/package/modules/class";
+import { propsModule } from "snabbdom/build/package/modules/props";
+import { styleModule } from "snabbdom/build/package/modules/style";
+import { eventListenersModule } from "snabbdom/build/package/modules/eventlisteners";
+import { h } from "snabbdom/build/package/h"; // helper function for creating vnodes
+import { VNode } from "snabbdom/build/package/vnode";
 
 /** HELPERS */
 
@@ -13,16 +14,12 @@ var patch = init([
   classModule, // makes it easy to toggle classes
   propsModule, // for setting properties on DOM elements
   styleModule, // handles styling on elements with support for animations
-  eventListenersModule // attaches event listeners
+  eventListenersModule, // attaches event listeners
 ]);
 
 const categories = ["None", "Fears", "Suggestions"];
 
-/**
- *
- * @param {Array<T>} xs
- */
-const randomSlice = (xs) => {
+const randomSlice = <T>(xs: T[]) => {
   const min = faker.random.number({ min: 0, max: xs.length - 1 });
   const max = faker.random.number({ min, max: xs.length - 1 });
 
@@ -42,13 +39,24 @@ const randomTag = () => ({
     { id: faker.random.uuid(), tag: faker.random.words() },
     { id: faker.random.uuid(), tag: faker.random.words() },
     { id: faker.random.uuid(), tag: faker.random.words() },
-    { id: faker.random.uuid(), tag: faker.random.words() }
-  ])
+    { id: faker.random.uuid(), tag: faker.random.words() },
+  ]),
 });
 
 /** APPLICATIONS */
 
-let state = null;
+type State = any;
+type Dispatcher = typeof dispatch;
+
+let state: State = null;
+let app: VNode | Element = document.querySelector("#container") as Element;
+
+const dispatch = (reducer: (action: unknown, state: State) => State) => (
+  event: Event
+) => {
+  state = reducer(event, state);
+  app = patch(app, view(dispatch, state));
+};
 
 const initState = () => ({
   response: categories.map((name, index) => ({
@@ -56,12 +64,12 @@ const initState = () => ({
     name,
     tags: Array(faker.random.number({ min: 3, max: 7 }))
       .fill(1)
-      .map(randomTag)
+      .map(randomTag),
   })),
-  isDragging: false // to grey-out lists while drag in progress
+  isDragging: false, // to grey-out lists while drag in progress
 });
 
-const hCategory = (d, category) =>
+const hCategory = (d: Dispatcher, category) =>
   h("div.category__container", [
     h("div.category__name", [category.name]),
     h(
@@ -69,24 +77,18 @@ const hCategory = (d, category) =>
       category.tags.map((t) =>
         h("div.tag", {}, [`${t.name} (${t.events.length})`])
       )
-    )
+    ),
   ]);
 
-const view = (d, state) =>
+const view = (d: Dispatcher, state: State) =>
   h(
-    "div.container",
+    "div#container",
     state.response.map((category) => hCategory(d, category))
   );
 
 const setup = () => {
   state = initState();
   console.log(state);
-  let app = document.querySelector(".container");
-
-  const dispatch = (reducer) => (event) => {
-    state = reducer(event, state);
-    app = patch(app, view(dispatch, state));
-  };
 
   app = patch(app, view(dispatch, state));
 };
